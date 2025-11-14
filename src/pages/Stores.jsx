0741e7d8286dcdb3c85b64226fbd35a4bbf239f6
sangnguyen2 <sangnguyen2@genesolutions.vn>
2025-11-14 17:42:09 +0700

    update stores

--- src/pages/Stores.jsx
+++ src/pages/Stores.jsx
@@ -21,15 +21,42 @@ export default function Products() {
     }, []);
 
     return (
-        <div>
-            <h1>Stores</h1>
+        <div className="pt-6">
+            <h1 className="font-bold text-3xl">Stores</h1>
             <div>
-                {content.length > 0 && content.map((item, index) => (
-                    <div key={index}>
-                        <h3>{item.storeName}</h3>
-                        <p>{item.location}</p>
-                    </div>
-                ))}
+                <table className="w-[100%] text-center">
+                    <thead>
+                        <tr className="bg-gray-300">
+                            <th className="border border-gray-400">STT</th>
+                            <th className="border border-gray-400">Cửa hàng</th>
+                            <th className="border border-gray-400">Vị trí</th>
+                            <th className="border border-gray-400">Liên hệ</th>
+                            <th className="border border-gray-400"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {content.length > 0 && content.map((item, index) => (
+                            <tr key={index}>
+                                <td className="border border-gray-400 py-1">
+                                    {item.storeId}
+                                </td>
+                                <td className="border border-gray-400 py-1">
+                                    {item.storeName}
+                                </td>
+                                <td className="border border-gray-400 py-1">
+                                    {item.location}
+                                </td>
+                                <td className="border border-gray-400 py-1">
+                                    {item.phoneNumber}
+                                </td>
+                                <td className="border border-gray-400 py-1">
+                                    <button className="border border-[var(--background4)] bg-[var(--background3)] px-6 rounded text-[var(--heading)] mr-2">Sửa</button>
+                                    <button className="border border-[var(--background4)] bg-[var(--heading)] px-6 rounded text-[var(--background3)]">Khóa</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
